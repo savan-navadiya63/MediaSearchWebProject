ab072ce1837e70bf8c0f3f518e8a26f4721cad99
savan <snavadiya600@gmail.com>
2026-02-27 16:04:12 +0530

    Fix: Correct Homepage import casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./components/pages/HomePage";
+import Homepage from "./components/pages/Homepage";
 import CollectionPage from "./components/pages/CollectionPage";
 import Navbar from "./components/Navbar";
 
@@ -8,7 +8,7 @@ const App = () => {
     <div className="min-h-screen w-full text-white bg-gray-950 overflow-x-hidden">
       <Navbar></Navbar>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/collection" element={<CollectionPage />} />
       </Routes>
     </div>
