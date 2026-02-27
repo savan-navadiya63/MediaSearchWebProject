60232c63f2aa41e37bce6a0c71a12a0929bc47ac
savan <snavadiya600@gmail.com>
2026-02-27 16:12:03 +0530

    Fix: Correct HomePage import casing for Linux/Netlify compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Homepage from "./components/pages/Homepage";
+import HomePage from "./components/pages/HomePage";
 import CollectionPage from "./components/pages/CollectionPage";
 import Navbar from "./components/Navbar";
 
@@ -8,7 +8,7 @@ const App = () => {
     <div className="min-h-screen w-full text-white bg-gray-950 overflow-x-hidden">
       <Navbar></Navbar>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/collection" element={<CollectionPage />} />
       </Routes>
     </div>
